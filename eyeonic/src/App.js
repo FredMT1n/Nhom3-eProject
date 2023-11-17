1e6627e54277ce179d5967ee328b9b2053010263
FredMT1n <minhhhng@gmail.com>
2023-11-17 16:40:22 +0700

    signin

--- eyeonic/src/App.js
+++ eyeonic/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom'
+import SignIn from './SignIn';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <div>
+        <Link to="/signin">Sign In</Link>
+      </div>
+      <Routes>
+        <Route path='/signin' element={<SignIn />}/>
+      </Routes>
     </div>
   );
 }
